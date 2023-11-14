97b06458907e4067c1e30235092c2cdefa5b1120
Felix <felix@DESKTOP-F0L63C2.localdomain>
2023-11-14 13:33:15 +0300

    page for all the transactions

--- bank-of-flatiron/src/components/App.js
+++ bank-of-flatiron/src/components/App.js
@@ -2,4 +2,19 @@ import React, { useState } from 'react';
 import TransactionList from './TransactionList';
 import NewTransactionForm from './NewTransactionForm';
 import CategoryFilter from './CategoryFilter';
-import { transactions } from './data';
+import { transactions } from './data'; 
+
+const App = () => {
+  const [allTransactions, setAllTransactions] = useState(transactions);
+  const [filteredTransactions, setFilteredTransactions] = useState(allTransactions);
+
+  const handleCategoryChange = (selectedCategory) => {
+    if (selectedCategory === 'All') {
+      setFilteredTransactions(allTransactions);
+    } else {
+      const filtered = allTransactions.filter(
+        (transaction) => transaction.category === selectedCategory
+      );
+      setFilteredTransactions(filtered);
+    }
+  };
