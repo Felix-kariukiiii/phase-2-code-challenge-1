f3d3ddb4570a22508fd76f3a85b1bbf84767965b
Felix <felix@DESKTOP-F0L63C2.localdomain>
2023-11-14 13:30:23 +0300

    linking all child components to the parent component

---
+++ bank-of-flatiron/src/components/App.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import TransactionList from './TransactionList';
+import NewTransactionForm from './NewTransactionForm';
+import CategoryFilter from './CategoryFilter';
+import { transactions } from './data';
